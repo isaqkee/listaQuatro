f3759ca69ed63b1a52d3c4cec1c765d24bdc2e11
isaqkee <141652864+isaqkee@users.noreply.github.com>
2024-05-27 08:41:25 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace listaQuatro
+{
+    class Projeto
+    {
+        static void Main(string[] args)
+        {
+            /*ATIVIDADE 1*/
+            /*Console.WriteLine("Informe um número para ver sua tabuada:");
+                int numero = int.Parse(Console.ReadLine());
+
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine(numero * i);
+                }*/
+
+            /*ATIVIDADE 2*/
+            /*Console.WriteLine("Digite um número:");
+                int numero = int.Parse(Console.ReadLine());
+
+                for (int i = 0; i <= numero; i += 3)
+                {
+                    Console.WriteLine(i);
+                }*/
+
+            /*ATIVIDADE 3*/
+            /*for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine("Digite um número:");
+                    int numero = int.Parse(Console.ReadLine());
+
+                    if (numero > 100)
+                    {
+                        Console.WriteLine("NÚMERO ESPECIAL");
+                    }
+                    else
+                    {
+                        Console.WriteLine("NÚMERO COMUM");
+                    }
+                }*/
+
+            /*ATIVIDADE 4*/
+            /*string resposta;
+
+            do
+            {
+                Console.WriteLine("Deseja continuar? (s/n)");
+                resposta = Console.ReadLine().ToLower();
+            } while (resposta == "s");
+
+            Console.WriteLine("Programa encerrado.");*/
+        }
+    }
+}
